75395226ae20d41fdbc94a0422e53d9e7f9c3a08
jeuskirchen <jeuskirchen@protonmail.com>
2025-05-31 12:48:41 +0200

    merge train scripts

--- scripts/train.py
+++ scripts/train.py
@@ -1,14 +1,14 @@
-import torch
-print(torch.cuda.is_available(), torch.cuda.device_count(), torch.cuda.get_device_name(0))
 import os
+import warnings
 
 import click
 import pytorch_lightning as pl
 import torch
 from omegaconf import OmegaConf
 from pytorch_lightning import callbacks, loggers, strategies
+import wandb
 
-from synformer.data.projection_dataset_new import ProjectionDataModule
+from synformer.data.projection_dataset_new import ProjectionDataModule  
 from synformer.models.wrapper import SynformerWrapper
 from synformer.utils.misc import (
     get_config_name,
@@ -17,10 +17,6 @@ from synformer.utils.misc import (
 )
 from synformer.utils.vc import get_vc_info
 
-torch.backends.cuda.matmul.allow_tf32 = True
-torch.backends.cudnn.allow_tf32 = True
-torch.set_float32_matmul_precision("medium")
-
 
 @click.command()
 @click.argument("config_path", type=click.Path(exists=True))
@@ -28,7 +24,7 @@ torch.set_float32_matmul_precision("medium")
 @click.option("--debug", is_flag=True)
 @click.option("--batch-size", "-b", type=int, default=196)
 @click.option("--num-workers", type=int, default=8)
-@click.option("--devices", type=int, default=4)
+@click.option("--devices", type=int, default=1)  # default=4
 @click.option("--num-nodes", type=int, default=int(os.environ.get("NUM_NODES", 1)))
 @click.option("--num-sanity-val-steps", type=int, default=1)
 @click.option("--log-dir", type=click.Path(dir_okay=True, file_okay=False), default="./logs")
@@ -59,6 +55,38 @@ def main(
     exp_name = get_experiment_name(config_name, vc_info.display_version, vc_info.committed_at)
     exp_ver = get_experiment_version()
 
+    if config.system.device == "gpu" and torch.cuda.is_available():
+        torch.backends.cuda.matmul.allow_tf32 = True
+        torch.backends.cudnn.allow_tf32 = True
+        torch.set_float32_matmul_precision("medium")
+
+    # Add flags (click options) to config for convenience
+    config.train.exp_name = exp_name
+    config.train.exp_ver = exp_ver
+    config.train.seed = seed
+    config.train.debug = debug
+    config.train.batch_size = batch_size
+    config.train.num_workers = num_workers
+    config.train.devices = devices
+    config.train.num_nodes = num_nodes
+
+    # Monitoring
+    if not os.path.exists("configs/wandb.yml"):
+        warnings.warn("No wandb config found! Not using wandb.")
+    else:
+        wandb_config = OmegaConf.load("configs/wandb.yml")
+        if wandb_config.enabled:
+            os.environ["WANDB_API_KEY"] = wandb_config["api_key"]
+            wandb.init(
+                project=config.project.name,
+                entity=wandb_config.entity,
+                sync_tensorboard=True,
+                config=OmegaConf.to_container(config), 
+                name=config.train.exp_name, 
+                save_code=False,
+                # resume=True
+            )
+
     # Dataloaders
     datamodule = ProjectionDataModule(
         config,
@@ -66,6 +94,7 @@ def main(
         num_workers=num_workers,
         **config.data,
     )
+    # print(datamodule)
 
     # Model
     model = SynformerWrapper(
@@ -79,6 +108,7 @@ def main(
             "resume": resume,
         },
     )
+    print(model)
 
     if resume:
         ckpt = torch.load(resume, map_location="cpu")
@@ -90,15 +120,29 @@ def main(
         }
         model.load_state_dict(filtered, strict=False)
         resume = None
-
-    strategy = strategies.DDPStrategy(static_graph=True, process_group_backend="gloo") if devices > 1 else "auto"
+    
+    # <temp>
+    # Checkpoint check: are they the same?
+    """
+    checkpoint = torch.load("data/trained_weights/sf_ed_default.ckpt", map_location="cpu") 
+    print(
+        "Checkpoint check:",
+        torch.allclose(
+            checkpoint["state_dict"]["model.decoder.in_token.weight"], 
+            model.state_dict()["model.decoder.in_token.weight"]
+        )
+    )
+    print(checkpoint["state_dict"]["model.decoder.in_token.weight"])
+    print(model.state_dict()["model.decoder.in_token.weight"])
+    """
+    # </temp>
 
     # Train
     trainer = pl.Trainer(
-        accelerator="gpu",
+        accelerator=config.system.device, 
         devices=devices,
         num_nodes=num_nodes,
-        strategy=strategy,
+        strategy=strategies.DDPStrategy(static_graph=True, process_group_backend="gloo") if devices > 1 else "auto",
         num_sanity_val_steps=num_sanity_val_steps,
         gradient_clip_val=config.train.max_grad_norm,
         log_every_n_steps=1,
@@ -113,7 +157,24 @@ def main(
         val_check_interval=config.train.val_freq,
         limit_val_batches=4,
     )
-    trainer.fit(model, datamodule=datamodule)
+    trainer.fit(
+        model, 
+        datamodule=datamodule
+    )
+    print("Finished training")
+
+    '''
+    if config.model.decoder.lora:
+        # Save LoRA weights 
+        #
+        #
+        #
+        # Merge LoRA weights into the original model weights
+        print("Merging...")
+        merged_decoder = model.model.decoder.lora_dec.merge_lora() 
+        # How to save inside of model? and delete lora_dec?
+        ...
+    '''
 
 
 if __name__ == "__main__":
